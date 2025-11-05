c5f432f51a51ba40f611a77d248484cfd9727ff0
GuerinoMJ <guerinomaio660@gmail.com>
2025-11-04 21:32:00 -0300

    feat: adiciona lista com 10 últimos repositórios

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,27 @@
+import { fetchGithubUser, fetchGithubUserRepos } from './githubApi.js';
+import { renderProfile } from './profileView.js';
+
 const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
-const profileResults = document.querySelector('.profile-results')
-
-const BASE_URL = 'https://api.github.com';
+const profileResults = document.querySelector('.profile-results');
 
 btnSearch.addEventListener('click', async () => {
     const userName = inputSearch.value;
-    if (userName) {
-        profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
-        try {
-            // aqui você pode adiciionar a lógica para usar o valor do input
-            const response = await fetch(`${BASE_URL}/users/${userName}`);
-
-            if (!response.ok) {
-                alert('Usuário não encontrado. Por favor, verifica o nome do usuário e tente novamente.');
-
-                profileResults.innerHTML = "";
-                return;
-            }
-
-            const userData = await response.json();
-            console.log(userData); // apenas para verificar se os dados foram obtidos corretamente
-
-            profileResults.innerHTML = `
-            <div class="profile-card">
-                <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
-
-                <div class="profile-info"> 
-                    <h2>${userData.name}</h2>
-                    <p>${userData.bio || 'Não possui bio cadastrada 😢.'}</p>
-                </div>
-            </div>`;
-
-        } catch (error) {
-            console.error('Erro ao buscar o perfil do usuário:', error);
-            alert('Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.');
-
-            profileResults.innerHTML = "";
-        }
-
-    } else {
-        alert('Por favor, digite um nome de usuário do GitHub');
+    if (!userName) {
+        alert('Por favor, digite um nome de usuário do GitHub.');
         profileResults.innerHTML = "";
+        return;
+    }
+    profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
+    try {
+        const userData = await fetchGithubUser(userName);
+        const userRepos = await fetchGithubUserRepos(userName);
+        console.log(userRepos);
 
+        renderProfile(userData, userRepos, profileResults);
+    } catch (error) {
+        console.error('Erro ao buscar o perfil do usuário:', error);
+        alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
+        profileResults.innerHTML = "";
     }
 });
---
+++ src/js/profileView.js
@@ -0,0 +1,45 @@
+export function renderProfile(userData, userRepos, container) {
+
+  const repositoriesHTML = userRepos && userRepos.length > 0 ? userRepos.map(repo => `
+    <a href="${repo.html_url}" target="blank">
+      <div class="repository-card">
+          <h3>${repo.name}</h3>
+          <div class="repository-stats">
+              <span>⭐ Stars: ${repo.stargazer_count}</span>
+              <span>🍴 Forks: ${repo.forks.count}</span>
+              <span>👀 Watchers: ${repo.watchers_count}</span>
+              <span>💻 Language: ${repo.lamguage || 'Não informado'}</span>
+          </div>
+      </div>
+    </a>
+`).join('') : `<p>Nenhum repositório encontrado.</p>`;
+
+  container.innerHTML = `
+<div class="profile-card">
+    <img src="${userData.avatar_url}" alt="Avatar de ${userData.name
+    }" class="profile-avatar">
+    <div class="profile-info">
+        <h2>${userData.name}</h2>
+        <p>${userData.bio || "Não possui bio cadastrada 😢."}</p>
+    </div>
+</div>
+
+<div class="profile-counters">
+    <div class="followers">
+        <h4>👥 Seguidores</h4>
+        <span>${userData.followers}</span>
+    </div>
+    <div class="following">
+        <h4>👥 Seguindo</h4>
+        <span>${userData.following}</span>
+    </div>
+</div>
+
+<div class="profile-repositories">
+    <h2>Repositório</h2>
+    <div class="repositories">
+        ${repositoriesHTML}
+    </div>
+</div>
+  `;
+}
